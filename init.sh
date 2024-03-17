6d6e633180395c94458a1db7263d44f53355c210
胡雨潇 <136304230+Xiao254182@users.noreply.github.com>
2024-03-17 14:42:40 +0800

    添加执行镜像监控脚本

--- init.sh
+++ init.sh
@@ -49,7 +49,8 @@ cd /root/dashboard && chmod -R 777 ./*
 npm install
 nohup npm run serve &
 
-chmod 777 /root/kvm.sh && chmod 777 /root/monitor.sh
-nohup bash monitor.sh & 
+chmod 777 /root/*.sh 
+nohup bash /root/monitor_kvm.sh & 
+nohup bash /root/monitor_iso.sh &
 
 echo "dashboard界面:ip地址:8081"
