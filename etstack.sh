0c291fe14e40725dc297c3654d5a1610df279a90
胡雨潇 <136304230+Xiao254182@users.noreply.github.com>
2024-03-19 08:50:52 +0800

    添加自动绑定对应网卡的功能

--- etstack.sh
+++ etstack.sh
@@ -7,7 +7,9 @@ cat >/usr/local/bin/etstack<<'EOF'
 
 if [ "$1" = "start" ]; then
     VM_NAME="$2"
-    virsh start "$VM_NAME" && brctl delif virbr0 vnet0 && brctl addif br1 vnet0
+    virsh start "$VM_NAME" 
+    NET_NAME=$(virsh dumpxml "$VM_NAME" | grep "vnet" | awk -F\' '/dev/ {print $2}')
+    brctl delif virbr0 "$NET_NAME" && brctl addif br1 "$NET_NAME"
 else
     echo "Usage: etstack start $VM_NAME"
 fi
